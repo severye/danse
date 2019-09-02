8daaac32e9b893b72ac918d84727d0f80ffd4453
Séverine <severine.bouillet@decathlon.com>
2019-09-02 12:50:13 +0200

    server

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./assets'));
 
-app.get('/', function(req,res) {
-    res.redirect('/search');
+app.get('*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
