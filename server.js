2565985b542e253970b4ded8987cad25260d4c98
Séverine <severine.bouillet@decathlon.com>
2019-09-02 12:24:30 +0200

    try with redirect

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./assets'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/', function(req,res) {
+    res.redirect('/search');
 });
 
 // Start the app by listening on the default Heroku port
